ba11792b2c5563d819d601ed7c47fd0367f12cd9
ymshkdev <masahiko.yonemura@outlook.jp>
2025-12-20 17:29:20 +0900

    デプロイの準備
    renderにあげるため

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Deviseのコントローラーが動くときに、パラメーター許可のメソッドを呼ぶ
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth
 
   private
 
@@ -9,5 +10,11 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     # アカウント編集(account_update)の際も許可
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '2222'
+      #username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 end
