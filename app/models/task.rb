4e41024b127949b026063176f593f449297af715
ymshkdev <masahiko.yonemura@outlook.jp>
2025-12-20 18:33:02 +0900

    taskバリデーション設定

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,13 @@
 class Task < ApplicationRecord
   belongs_to :user
-
+  # タイトルを必須（空欄禁止）にし、最大50文字の制限
+  validates :title, presence: true, length: { maximum: 50 }
+  # メモ（description）は必須ではないので、記載しない
+  # 優先度やステータスも必須にしたい場合は追加する
+  validates :priority, presence: true
+  validates :status, presence: true
   # priorityカラムの 0, 1, 2 を定義
   enum priority: { low: 0, middle: 1, high: 2 }, _prefix: true
+  #statusの実装で使用
+  #enum status: { todo: 0, doing: 1, done: 2 }
 end
