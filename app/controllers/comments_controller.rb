32d2f911e25d94114c4d2ead52fe25dff7e39615
ymshkdev <masahiko.yonemura@outlook.jp>
2025-12-25 13:26:09 +0900

    コメント機能の実装

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,32 @@
+class CommentsController < ApplicationController
+  def create
+    @task = Task.find(params[:task_id])
+    @comment = @task.comments.build(comment_params)
+    @comment.user = current_user # ログインユーザーを紐付け
+
+    if @comment.save
+      redirect_to task_path(@task), notice: "コメントを投稿しました"
+    else
+      redirect_to task_path(@task), alert: "コメントの投稿に失敗しました"
+    end
+  end
+
+  def destroy
+  @task = Task.find(params[:task_id])
+  @comment = @task.comments.find(params[:id])
+
+  # 権限チェック：コメントの作成者本人、またはチームのリーダーであれば削除可能
+  if @comment.user == current_user || @task.teams.any? { |team| current_user.leader_of?(team) }
+    @comment.destroy
+    redirect_to task_path(@task), notice: "コメントを削除しました。", status: :see_other
+  else
+    redirect_to task_path(@task), alert: "権限がありません。"
+  end
+end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,9 @@ Rails.application.routes.draw do
   resources :teams do
     resources :memberships, only: [:create, :destroy]
   end
-  resources :tasks
+  resources :tasks do
+    resources :comments, only: [:create, :destroy]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
